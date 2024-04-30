47d83e2bf781162c73d9945c75d33ab376df026f
ZuuKkaa <zgvelesiani8@gmail.com>
2024-04-30 22:07:56 +0400

    first commit

---
+++ GOA/Day 01/Homeworks/1lesson1.py
@@ -0,0 +1,32 @@
+name = "zuka"
+#name არის ვცლადი
+#= არის ვცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#"zuka" არის ცვლადისთვის მნიშვნელობა
+surname = "gvelesiani"
+
+#print(name)
+#პრინტ ფუქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "zuka" #ეს არის str (string) ტიპის ვცვლადი
+age = 15 # ეს არის int ( intenger ) მთელი რიცხვი
+heigh = 180 #ეს არის float ტიპის ვცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing = True #true ან false
+is_ugly = False  #snackecase( უბრალოდ წერის სტილის სტანდარტულად )
+ 
+isUgly = False # ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(heigh))
+#print(type(knows_programing))
+
+print(str(age)+ " წლის ")
