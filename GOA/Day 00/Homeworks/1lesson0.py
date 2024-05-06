c3de258f1db2310c4c69f66c669218fd3d9821ed
ZuuKkaa <zgvelesiani8@gmail.com>
2024-05-06 14:08:29 +0400

    second commit

--- GOA/Day 00/Homeworks/1lesson0.py
+++ GOA/Day 00/Homeworks/1lesson0.py
@@ -1,49 +1,106 @@
 from turtle import *
+speed (30)
+width(4)
 
-
-#we want to paint a house
-#step 1:  draw a square
-speed(30)
-color("purple")
-width(7)
-forward(200)
+# კვადრატი
+color("brown")
+begin_fill()      
+forward(220)
 left(90)
 
-forward(200)
+forward(220)
 left(90)
 
-forward(200)
+forward(220)
 left(90)
 
-forward(200)
+forward(220)
 left(90)
-#end of square
+end_fill()
 
-#drawing a door
-
-forward(70)
-color("black")
+# კარები
+forward(80)
+color("gray")
+begin_fill()
 left(90)
-forward(120)      #height of the door
+forward(120)
 right(90)
 forward(60)
 right(90)
 forward(120)
-
-penup()
-goto(200, 200)
-pendown()
-
-color("red")
-begin_fill()
-right(150)
-forward(200)
-left(120)
-forward(200)
 end_fill()
 
+penup()
+goto(220, 220)
+pendown()
 
+#სახურავი
+color("saddle brown")
+begin_fill()
+right(140)
+forward(160)
+left(95)
+forward(170)
+end_fill()
 
+# ფანჯარა 1
+color("cyan")
+begin_fill()
+penup()
+goto(20, 140)
+pendown()
+right(135)
+forward(60)
+right(90)
+forward(60)
+right(90)
+forward(60)
+right(90)
+forward(60)
+end_fill()
+right(90)
+forward(30)
+right(90)
+color("white")
+forward(60)
+color("cyan")
+left(90)
+forward(30)
+left(90)
+forward(30)
+left(90)
+color("white")
+forward(60)
+
+# ფანჯარა 2
+color("cyan")
+begin_fill()
+penup()
+goto(140, 140)
+pendown()
+left(90)
+left(90)
+forward(60)
+right(90)
+forward(60)
+right(90)
+forward(60)
+right(90)
+forward(60)
+end_fill()
+right(90)
+forward(30)
+right(90)
+color("white")
+forward(60)
+color("cyan")
+left(90)
+forward(30)
+left(90)
+forward(30)
+left(90)
+color("white")
+forward(60)
 
 
 exitonclick()
--- GOA/Day 01/Homeworks/1lesson1.py
+++ GOA/Day 01/Homeworks/1lesson1.py
@@ -1,32 +1,7 @@
-name = "zuka"
-#name არის ვცლადი
-#= არის ვცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
-#"zuka" არის ცვლადისთვის მნიშვნელობა
-surname = "gvelesiani"
+name = "My name is Zuka"
+surname = "My surname is Gvelesiani"
+height = "I am 180"
+story = "I joined GOA because i want to increase and upgrade my skils"
+age = "I am 15 years old"
 
-#print(name)
-#პრინტ ფუქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
-
-name = "zuka" #ეს არის str (string) ტიპის ვცვლადი
-age = 15 # ეს არის int ( intenger ) მთელი რიცხვი
-heigh = 180 #ეს არის float ტიპის ვცვლადი ( ათწილადი )
-#boolean (bool) ტიპის ცვლადი
-
-knows_programing = True #true ან false
-is_ugly = False  #snackecase( უბრალოდ წერის სტილის სტანდარტულად )
- 
-isUgly = False # ჯავასკრიპტული camelcase
-
-
-print(name + " " + surname)
-
-#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
-#print(name + age)
-
-#print(type(age))
-#print(type(name))
-#print(type(surname))
-#print(type(heigh))
-#print(type(knows_programing))
-
-print(str(age)+ " წლის ")
+print(name + " " + surname + " " + height + " " + story + " " + age)
