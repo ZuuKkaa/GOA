1a5b7f29e119e0cca80ddea6e3a24ca3ad894f99
ZuuKkaa <zgvelesiani8@gmail.com>
2024-05-28 08:41:42 +0400

    GOA

---
+++ GOA/Level 05/Homework/homework.py
@@ -0,0 +1,26 @@
+name = "My name is Zuka"
+surname = "My surname is Gvelesiani"
+height = "I am 180cm tall"
+address = "I live in Zestafoni"
+age = "I am 15 years old"
+
+print(name + " " + surname + " " + height + " " + address + " " + age  )
+
+
+
+print(10 + 10)
+print(15 + 15)
+#ეს არის ორი ცვლადის შეკრება
+print(40 - 20)
+print(90 - 40)
+#ეს არი ორი ცვლადის სხვაობა
+print(10 * 10)
+print(50 * 10)
+print(77 * 20)
+print(12 * 20)
+#ეს არის ორი ცვლადის გამრავლება
+print(120 / 20)
+print(100 / 10)
+print(90 / 30)
+print(60 / 20)
+#ეს არის ორი ცვლადის გაყოფა
