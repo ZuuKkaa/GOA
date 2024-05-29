b8c91d573e58aec0ebfdc772ec5ea27718b5b613
ZuuKkaa <zgvelesiani8@gmail.com>
2024-05-29 21:37:21 +0400

    GOA

---
+++ GOA/Level 06/Classwork/classwork.py
@@ -0,0 +1,23 @@
+#შემოვიტანოთ ორი ცვლადი
+#num1 = input("enter a number: ")
+#num2 = input("enter a second number: ")
+#გამოვთვალოთ ორი ცვლადის დამატება
+#print(int(num1) + int(num2))
+#გამოვთვალოთ ორი ცვლადის სხვაობა
+#print(int(num1) - int(num2))
+#გამოვთვალოთ ორი ცვლადის გამრავლება
+#print(int(num1) * int(num2))
+#გამოვთვალოთ ორი ცვლადის გაყოფა
+#print(int(num1) / int(num2))
+
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# print(int(num1) + int(num2))
+
+
+name = input("enter your name: ")
+surname = input("enter your surname: ")
+email = input("enter your email: ")
+age = input("enter your age: ")
+print(f" Name: {name}, surname: {surname}, email: {email}, age: {age}")
---
+++ GOA/Level 06/Homework/homework.py
@@ -0,0 +1,25 @@
+# #შემოვიტანოთ ორი ცვლადი
+# num1 = input("enter a number: ")
+# num2 = input("enter a second number: ")
+# #გამოვთვალოთ ორი ცვლადის დამატება
+# print(int(num1) + int(num2))
+# #გამოვთვალოთ ორი ცვლადის სხვაობა
+# print(int(num1) - int(num2))
+# #გამოვთვალოთ ორი ცვლადის გამრავლება
+# print(int(num1) * int(num2))
+# #გამოვთვალოთ ორი ცვლადის გაყოფა
+# print(int(num1) / int(num2))
+
+# name= input("enter your name: ")
+# print("gilocav" + " " + name + " " + "shen moige...")
+
+
+# name = input("enter your name: ")
+# surname = input("enter your surname: ")
+# age = input("enter your age: ")
+# print(f"Name: {name}, surname: {surname}, age: {age}.")
+
+
+num = int(input("Enter your age: "))
+num = num + 25
+print("After 25 years, You will be: " + str(num))
