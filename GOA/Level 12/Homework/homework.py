fcd99bbf755794ebb5cb29438a4db73dca7808d4
ZuuKkaa <zgvelesiani8@gmail.com>
2024-06-29 14:25:18 +0400

    GOA

---
+++ GOA/Level 12/Homework/homework.py
@@ -0,0 +1,30 @@
+#! Level easy
+
+
+# vkitxot momxamrebels asaki
+
+age = int(input("Enter your age: "))
+
+
+
+# shevamowmot asaki sworia tu araswori
+if age > 0:
+    print("The age entered is positive.")
+elif age < 0:
+    print("The age entered is negative.")
+else:
+    print("The age entered is zero, which is not valid.")
+
+
+
+
+# vkitxot momxamrebels ricxvi
+number = int(input("Enter a number: "))
+
+
+
+# vnaxot  ricxvi udris 10s
+if number == 10:
+    print("The number entered is equal to 10.")
+else:
+    print("The number entered is not equal to 10.")
---
+++ GOA/Level 12/Homework/homework5.py
@@ -0,0 +1,21 @@
+#! Level easy
+
+
+
+number = 1
+while number <= 5:
+    print(number)
+    number += 1
+
+
+
+
+print("-------------")
+
+
+
+
+count = 0
+while count < 3:
+    print("Hello")
+    count += 1
